0fa7b1743bbbb4dffd93faa1730f5900f75e181e
github-action-benchmark <github@users.noreply.github.com>
2021-07-30 13:54:02 +0000

    add DataPrep.EDA Benchmarks (pytest) benchmark result for c5d6a4b9447a4b7219909daac05520fc139bbf82

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1627653241114,
+  "repoUrl": "https://github.com/crayon/dataprep",
+  "entries": {
+    "DataPrep.EDA Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "jillian.augustine@crayon.com",
+            "name": "Jill Augustine",
+            "username": "jill-augustine"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c5d6a4b9447a4b7219909daac05520fc139bbf82",
+          "message": "Merge pull request #1 from sfu-db/develop\n\nMerging latest commits from original dataprep repo",
+          "timestamp": "2021-07-30T15:50:32+02:00",
+          "tree_id": "b1eb050b9d5305e56f83e37355d96cdc45c0b860",
+          "url": "https://github.com/crayon/dataprep/commit/c5d6a4b9447a4b7219909daac05520fc139bbf82"
+        },
+        "date": 1627653239553,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "dataprep/tests/benchmarks/eda.py::test_create_report",
+            "value": 0.15961481475556502,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0989783721214669",
+            "extra": "mean: 6.265082608599994 sec\nrounds: 5"
+          }
+        ]
+      }
+    ]
+  }
+}
